aced6766fbfd106ea017f57d356e18714d44cdb7
Zaki Ahmad <zakiahmad42661@gmail.com>
2023-12-19 12:35:57 -0500

    added logo and favicon

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import LoginButton from './LoginButton'
+import LoginButton from "./LoginButton";
+import { ReactComponent as Logo } from "../icons/logo.svg";
 
 const Nav = () => {
   return (
-    <nav className='text-white w-screen px-4'>
-      <div className='flex justify-between'>
-        <span className='text-orange-500 font-bold text-lg'>
-          Rep Record
-        </span>
-          <LoginButton/>
+    <nav className="text-white w-screen px-4">
+      <div className="flex justify-between">
+        <div className="-m-4">
+          <Logo className="h-20 w-20 md:h-36 md:w-36" /> 
+        </div>
+        <div className="">
+          <LoginButton />
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
